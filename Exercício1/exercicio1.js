aff8696416172c8139569981d7865e05837882f7
Anderson Lima <anderson.lima@sidia.com>
2019-12-17 15:35:27 -0400

    API finalizada
    Signed-off-by: Anderson Lima <anderson.lima@sidia.com>

--- Exercício1/exercicio1.js
+++ Exercício1/exercicio1.js
@@ -1,13 +1,63 @@
 var nomeTurma = prompt("Informe o nome da turma");
 
+var turma;
+
 $.ajax({
     url: "http://localhost:3002/turma?nome=" + nomeTurma,
     success: function(response){
         console.info(response);
+        if(response.length == 0){
+            turma = new Turma(nomeTurma);
+            salvarTurma();
+        }else{
+            turma = new Turma(response[0].nome);
+            turma._id = response[0]._id;
+
+            response[0].alunos.map(aluno => {
+                turma.adicionarAluno(new Aluno(aluno.nome + " " + aluno.sobrenome, aluno.dataNascimento));
+            });
+            listaAlunos();
+
+            console.info(turma);
+        }
     }
 });
 
-var turma = new Turma(nomeTurma);
+function salvarTurma(){
+    var data = JSON.stringify(turma);
+
+    $.ajax({
+        url: "http://localhost:3002/turma",
+        method: "POST",
+        data: data,
+        contentType: "application/json; charset=utf-8",
+        success(response){
+            turma._id = response._id;
+            console.info("Turma salva com sucesso");
+        },
+        error(){
+            console.error("Falha ao inserir turma");
+        }
+    });
+}
+
+function atualizarTurma(){
+    var data = JSON.stringify(turma);
+    console.info(turma);
+
+    $.ajax({
+        url: "http://localhost:3002/turma/" + turma._id,
+        method: "PUT",
+        data: data,
+        contentType: "application/json; charset=utf-8",
+        success(){
+            console.info("Turma atualizada com sucesso");
+        },
+        error(){
+            console.error("Falha ao atualizar turma");
+        }
+    });
+}
 
 var inputNome;
 var inputData;
@@ -27,6 +77,8 @@ function inserirAluno(){
     var novoAluno = new Aluno( inputNome.value, inputData.value );
 
     turma.adicionarAluno(novoAluno);
+    atualizarTurma();
+
     numeroAlunos.innerHTML = turma.numeroAlunos;
 
     console.info(turma);
@@ -48,6 +100,8 @@ function procurarAluno(){
 function listaAlunos(){
 
     $("#listaAlunos").children().remove();
+
+    $('#numeroAlunos').html(turma.numeroAlunos);
 
     turma.alunos.map(aluno => {
         $("#listaAlunos").append("<h1>" + aluno.nomeCompleto + "</h1>");
--- server/controllers/turmaController.js
+++ server/controllers/turmaController.js
@@ -25,16 +25,23 @@ exports.listarTurmas = function(request, response){
 }
 
 exports.inserirTurma = function(request, response){
+    console.info("POST body");
+    console.info(request.body);
+
     var novaTurma = new Turma(request.body);
     novaTurma.save(function(error, result){
         if(error){
             response.send("Erro ao salvar turma");
         }
-        response.send("Turma criada com sucesso");
+        response.send(result._id);
     });
 }
 
 exports.atualizarTurma = function(request, response){
+    console.info("UPDATE TURMA");
+    console.info(request.params.id);
+    console.info(request.body);
+    
     Turma.findOneAndUpdate({_id: request.params.id}, request.body, function(error, result){
         if(error){
             response.send("Erro ao atualizar turma");
